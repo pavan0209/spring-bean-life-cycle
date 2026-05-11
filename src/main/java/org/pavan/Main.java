1879a703a099443337835e7f14d58ee20edb668a
pavan0209 <pavanasonawane@gmail.com>
2026-05-11 22:02:59 +0530

    initial commit

---
+++ src/main/java/org/pavan/Main.java
@@ -0,0 +1,13 @@
+package org.pavan;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+
+    }
+}
